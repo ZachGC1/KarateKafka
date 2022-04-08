1c49fa2bfccc50ce4a09bf2455ef1ba408d0e641
Anrich Hildebrand <AnrichH@agilebridge.co.za>
2022-04-08 12:47:56 +0200

    Quick transfer

---
+++ Centrifugo/AgileBridge.KarateKafka.Centrifugo/BombSquad/ReleaseMoves.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombSquad
+{
+    public  class ReleaseMoves
+    {
+        private static List<User>
+        public static void CreateUserMove()
+        {
+
+        }
+    }
+}
--- Centrifugo/AgileBridge.KarateKafka.Centrifugo/Program.cs
+++ Centrifugo/AgileBridge.KarateKafka.Centrifugo/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BombSquad.Models;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using Newtonsoft.Json;
+
+using var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:9092/"),
+    Timeout = TimeSpan.FromMilliseconds(10000)
+};
+
+UserMove userMove = JsonConvert.DeserializeObject<UserMove>(System.IO.File.ReadAllText(@"C:\Repos\KarateKafka\SharedData\data.json"));
+
+var step = Step.Create("bomb_publisher", async context =>
+{
+    try
+    {
+
+        var response = await httpClient.PostAsync("api", );
+
+        return response.IsSuccessStatusCode
+            ? Response.Ok()
+            : Response.Fail();
+
+    }
+    catch (Exception ex)
+    {
+        return Response.Fail();
+    }
+});
