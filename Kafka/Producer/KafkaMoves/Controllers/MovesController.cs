6a5379b9543dfbed84b56b9050c93a33b1789dac
Wayne Meyer <waynem@agilebridge.co.za>
2022-04-08 14:29:04 +0200

    wym - quick api for handling moves

---
+++ Kafka/Producer/KafkaMoves/Controllers/MovesController.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaMoves.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovesController : ControllerBase
+    {
+        ProducerConfig config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        string myId = Guid.NewGuid().ToString();
+
+        [HttpPost]
+        public async Task<string> UserMove(string move)
+        {
+            using var p = new ProducerBuilder<string, string>(config).Build();
+            {
+                var message = new Message<string, string>()
+                {
+                    Key = myId,
+                    Value = move
+                };
+
+                var dr = await p.ProduceAsync("guid", message);
+                return $"Produced message on key {message.Key} to topic {dr.Topic}, partition {dr.Partition}, offset {dr.Offset}";
+            }
+        }
+    }
+}
