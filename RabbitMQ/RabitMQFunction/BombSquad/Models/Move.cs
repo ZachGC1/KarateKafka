8743df9179a6019a60a6688810b06c57db00eac1
Joanita <joanitan@agilebridge.co.za>
2022-04-08 12:32:29 +0200

    BombSquad init

---
+++ RabbitMQ/RabitMQFunction/BombSquad/Models/Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombSquad.Models
+{
+    public class Move
+    {
+        public Move(
+           String name,
+           String english,
+           Int32 level)
+        {
+            Name = name;
+            English = english;
+            Level = level;
+        }
+        public String Name { get; set; }
+        public String English { get; set; }
+        public Int32 Level { get; set; }
+    }
+}
---
+++ RabbitMQ/RabitMQFunction/BombSquad/Program.cs
@@ -0,0 +1,27 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+
+using var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:9092/"),
+    Timeout = TimeSpan.FromMilliseconds(10000)
+};
+
+var step = Step.Create("bomb_publisher", async context =>
+{
+    try
+    {
+        System.IO.File.ReadAllLines();
+
+        var response = await httpClient.PostAsync("api/publish", );
+
+        return response.IsSuccessStatusCode
+            ? Response.Ok()
+            : Response.Fail();
+
+    }
+    catch (Exception ex)
+    {
+        return Response.Fail();
+    }
+});
